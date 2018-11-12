70a1384cfb0a7b2055ed4132b3cc0ac9e11ea341
Nicolas Useche <nico311021@gmail.com>
2018-11-12 00:08:18 -0500

    Modificación de BottomNavigationBar y tema de MainActivity.

--- app/src/main/java/com/demo/nicolas/mytransport/MainActivity.java
+++ app/src/main/java/com/demo/nicolas/mytransport/MainActivity.java
@@ -1,54 +1,57 @@
 package com.demo.nicolas.mytransport;
 
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigation_inicio:
+                    Log.d("ITEM_SELECCIONADO", " === Inico ===");
                     //Código del fragment
-                    setTitle("Pagina Uno");
+                    setTitle("Inicio");
                     PaginaUno fragment = new PaginaUno();
                     FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
                     fragmentTransaction.replace(R.id.container,fragment,"FragmentName");
                     fragmentTransaction.commit();
                     return true;
 
-                case R.id.navigation_dashboard:
+                case R.id.navigation_valoraciones:
+                    Log.d("ITEM_SELECCIONADO", " === Mis Valoraciones ===");
                     //Código del fragment
-                    setTitle("Pagina Dos");
+                    setTitle("Mis Valoraciones");
                     PaginaDos fragment2 = new PaginaDos();
                     FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
                     fragmentTransaction2.replace(R.id.container,fragment2,"FragmentName");
                     fragmentTransaction2.commit();
                     return true;
 
-                case R.id.navigation_notifications:
+                case R.id.navigation_mapa:
+                    Log.d("ITEM_SELECCIONADO", " === Mapa ===");
                     //Código del fragment
-                    setTitle("Pagina Tres");
+                    setTitle("Mapa");
                     PaginaTres fragment3 = new PaginaTres();
                     FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
                     fragmentTransaction3.replace(R.id.container,fragment3,"FragmentName");
                     fragmentTransaction3.commit();
                     return true;
 
-                case R.id.mi_perfil:
+                case R.id.navigation_perfil:
+                    Log.d("ITEM_SELECCIONADO", " === Mi perfil ===");
                     //Código del fragment
-                    setTitle("Pagina cuatro");
+                    setTitle("Mi perfil");
                     PaginaCuatro fragment4 = new PaginaCuatro();
                     FragmentTransaction fragmentTransaction4 = getSupportFragmentManager().beginTransaction();
                     fragmentTransaction4.replace(R.id.container,fragment4,"FragmentName");
@@ -59,16 +62,16 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        //3:06 Fragment
-
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
 
         //Código del fragment
         setTitle("Pagina Uno");
@@ -79,3 +82,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
