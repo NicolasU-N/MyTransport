c7ea9dfc7cfa070f677fbee8b9abd31875dedadf
Nicolas Useche <nico311021@gmail.com>
2018-11-18 22:40:42 -0500

    Modificación fragment MainActivity.

--- app/src/main/java/com/demo/nicolas/mytransport/MainActivity.java
+++ app/src/main/java/com/demo/nicolas/mytransport/MainActivity.java
@@ -1,15 +1,11 @@
 package com.demo.nicolas.mytransport;
 
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,41 +17,33 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_inicio:
                     Log.d("ITEM_SELECCIONADO", " === Inico ===");
                     //Código del fragment
-                    setTitle("Inicio");
-                    PaginaUno fragment = new PaginaUno();
-                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction.replace(R.id.container,fragment,"FragmentName");
-                    fragmentTransaction.commit();
+                    setTitle("Inicio"); //Título AppBar
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,new PaginaUno(),"FragmentName")
+                            .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out).commit();
                     return true;
 
                 case R.id.navigation_valoraciones:
                     Log.d("ITEM_SELECCIONADO", " === Mis Valoraciones ===");
                     //Código del fragment
                     setTitle("Mis Valoraciones");
-                    PaginaDos fragment2 = new PaginaDos();
-                    FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction2.replace(R.id.container,fragment2,"FragmentName");
-                    fragmentTransaction2.commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,new PaginaDos(),"FragmentName")
+                            .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out).commit();
                     return true;
 
                 case R.id.navigation_mapa:
                     Log.d("ITEM_SELECCIONADO", " === Mapa ===");
                     //Código del fragment
                     setTitle("Mapa");
-                    PaginaTres fragment3 = new PaginaTres();
-                    FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction3.replace(R.id.container,fragment3,"FragmentName");
-                    fragmentTransaction3.commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,new PaginaTres(),"FragmentName")
+                            .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out).commit();
                     return true;
 
                 case R.id.navigation_perfil:
                     Log.d("ITEM_SELECCIONADO", " === Mi perfil ===");
                     //Código del fragment
                     setTitle("Mi perfil");
-                    PaginaCuatro fragment4 = new PaginaCuatro();
-                    FragmentTransaction fragmentTransaction4 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction4.replace(R.id.container,fragment4,"FragmentName");
-                    fragmentTransaction4.commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,new PaginaCuatro(),"FragmentName")
+                            .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out).commit();
                     return true;
             }
             return false;
@@ -74,11 +62,9 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Código del fragment
-        setTitle("Pagina Uno");
-        PaginaUno fragment = new PaginaUno();
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.container,fragment,"FragmentName");
-        fragmentTransaction.commit();
+        setTitle("Inicio"); //Título AppBar
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,new PaginaUno(),"FragmentName")
+                .commit();
     }
 
 }
